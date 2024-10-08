9df85f475e76f5f052f20029baea057438a7a164
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-08 19:25:04 -0300

    .

--- jaview_backend/src/app.ts
+++ jaview_backend/src/app.ts
@@ -20,5 +20,5 @@ app.use("/auth", authRoutes);
 app.use("/post", movieRoutes);
 
 app.listen(3333, () => {
-  console.log(`Server is listening in port ${PORT}`);
+  console.log(`Server is listening in port ${PORT} 🚀`);
 });
