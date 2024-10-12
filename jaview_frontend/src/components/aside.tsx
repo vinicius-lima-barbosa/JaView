a8ccef47548e79d26ff00e4caa507401e0d325f2
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-12 19:42:12 -0300

    feat: add responsive layout

--- jaview_frontend/src/components/aside.tsx
+++ jaview_frontend/src/components/aside.tsx
@@ -1,12 +1,27 @@
-import avatar from "../../public/images/default_avatar.jpg";
+import { useState } from "react";
+import avatar from "../images/default_avatar.jpg";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Aside() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <aside className="w-1/5 bg-gray-800 p-4 min-h-screen">
+      <button
+        className="block lg:hidden p-4 text-gray-400 focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <AiOutlineMenu size={24} />
+      </button>
+
+      <aside
+        className={`${
+          isOpen ? "block" : "hidden"
+        } lg:block lg:w-1/5 bg-gray-800 p-4 min-h-screen absolute lg:relative z-50 w-full`}
+      >
         <div className="flex items-center m-2 mb-8">
           <img src={avatar} alt="avatar" className="rounded-full mr-4 w-14" />
-          <span>Hi, user</span>
+          <span className="text-white">Hi, user</span>
         </div>
         <nav>
           <ul>
