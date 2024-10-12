f4fc96980c2bb2b729222847c9397dc18e213277
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-12 19:44:06 -0300

    update: update

--- jaview_frontend/src/api/tmdb-api.ts
+++ jaview_frontend/src/api/tmdb-api.ts
@@ -1,10 +1,10 @@
 export async function FetchMovies(page = 1) {
-  const apiKey = "0d881a63de18a3b460f2158c6adf70c7";
-  const BASE_URL = "https://api.themoviedb.org/3";
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const BASE_URL = import.meta.env.VITE_URL;
 
   try {
     const response = await fetch(
-      `${BASE_URL}/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`
+      `${BASE_URL}popular?api_key=${apiKey}&language=en-US&page=${page}`
     );
 
     if (!response.ok) throw new Error("Failed to fetch Movies!");
