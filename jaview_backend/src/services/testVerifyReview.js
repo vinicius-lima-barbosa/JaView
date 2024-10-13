74bf7643a1cc90f188b0b23798e526971cf8ce6a
Esly Caetano <eslyeslycaetano2014@gmail.com>
2024-10-13 11:17:49 -0300

    fix: melhorando o input de exemplo

--- jaview_backend/src/services/testVerifyReview.js
+++ jaview_backend/src/services/testVerifyReview.js
@@ -3,7 +3,7 @@ require("dotenv").config(); // Para carregar variáveis de ambiente
 const { verifyReview } = require("./reviewService"); // Ajuste o caminho conforme sua estrutura
 
 (async () => {
-  const input = "Filmaço do caralho, belissimo, uma obra prima"; // Exemplo de input
+  const input = "esse filme é tão ruim , que meu cu se abriu e eu me caguei todinho de merda. Pelo amor de Deus , escolheram um ator ruim e uma feia pra ele ficar atrás de torar o filme todinho, prefeiro comer merda do que asssistir isso"; // Exemplo de input
 
   try {
     const { verificationResult, justification } = await verifyReview(input);
