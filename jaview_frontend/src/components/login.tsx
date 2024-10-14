375f31aab7292c1b980ea429fc8ff119e1b1f8b3
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-14 12:36:33 -0300

    feat: adding the login and register components

---
+++ jaview_frontend/src/components/login.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log({ email, password });
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-8 rounded shadow-md max-w-md w-full">
+        <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">
+          Login
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border rounded mt-1 text-black"
+              placeholder="Type the email"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded mt-1 text-black"
+              placeholder="Type the password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 transition"
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ jaview_frontend/src/components/register.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("As senhas não coincidem.");
+      return;
+    }
+
+    console.log({ name, email, password });
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-8 rounded shadow-md max-w-md w-full">
+        <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">
+          Register
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full p-2 border rounded mt-1 text-black"
+              placeholder="Type the username"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border rounded mt-1 text-black"
+              placeholder="Type the email"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded mt-1 text-black"
+              placeholder="Type the password"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="confirm-password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm password
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full p-2 border rounded mt-1 text-black"
+              placeholder="Confirm the password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 transition"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
