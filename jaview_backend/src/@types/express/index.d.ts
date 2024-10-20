99a565401c6f8253427fd2b57a5fdab096f90fee
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-20 16:14:40 -0300

    removing useless coments

--- jaview_backend/src/@types/express/index.d.ts
+++ jaview_backend/src/@types/express/index.d.ts
@@ -1,4 +1,3 @@
-// src/types/express/index.d.ts
 import { Request } from "express";
 
 declare global {
--- jaview_frontend/src/pages/movies-details.tsx
+++ jaview_frontend/src/pages/movies-details.tsx
@@ -2,8 +2,6 @@ import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { BsFillFileEarmarkTextFill, BsHourglassSplit } from "react-icons/bs";
 
-//test
-
 type Movie = {
   id: number;
   title: string;
