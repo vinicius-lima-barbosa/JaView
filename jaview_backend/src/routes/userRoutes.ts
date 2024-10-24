42d3e7a860d78708431d6f87f5ed7c51b2ea4dc0
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-24 18:48:39 -0300

    feat: add routes for get and update profile

--- jaview_backend/src/routes/userRoutes.ts
+++ jaview_backend/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
 import {
+  getUserByUsernameController,
   getUserProfileController,
   getUserReviewsController,
+  updateUserProfileController,
 } from "../controller/userController";
 
 const router = Router();
@@ -10,5 +12,17 @@ const router = Router();
 router.get("/reviews", authMiddleware as any, getUserReviewsController as any);
 
 router.get("/profile", authMiddleware as any, getUserProfileController as any);
+
+router.get(
+  "/search-user",
+  authMiddleware as any,
+  getUserByUsernameController as any
+);
+
+router.put(
+  "/update-profile",
+  authMiddleware as any,
+  updateUserProfileController as any
+);
 
 export default router;
