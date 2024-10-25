2b18d54428e4655f73bcaa43e85e337d176a1681
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-25 11:35:36 -0300

    style: better style in seatch for user page

--- jaview_frontend/src/pages/search-user.tsx
+++ jaview_frontend/src/pages/search-user.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { User } from "../types/user-type";
+import { IoIosSearch } from "react-icons/io";
 
 const API_BACKEND = import.meta.env.VITE_BACKEND;
 
 export default function SearchUser() {
   const [searchUser, setSearchUser] = useState("");
   const [results, setResults] = useState<User[]>([]);
+  const navigate = useNavigate();
 
   const handleSearch = async () => {
     if (searchUser.trim() === "") return;
@@ -22,46 +24,50 @@ export default function SearchUser() {
           },
         }
       );
+
       const data = await response.json();
       console.log(data);
       setResults(data.users || []);
     } catch (error) {
       console.error("Error searching for users:", error);
+      navigate("/error", { state: { message: "An error occurred!" } });
     }
   };
 
   return (
-    <div className="container mx-auto py-8">
-      <h2 className="text-2xl font-bold mb-4">Search Users</h2>
-      <div className="flex items-center gap-2">
+    <div className="min-h-screen flex flex-col items-center py-10">
+      <h2 className="text-3xl font-bold mb-6 text-center">Search Users</h2>
+      <div className="flex items-center overflow-hidden transition-all duration-300 ease-in-out bg-white rounded-full shadow-md mr-4 p-1 mb-4">
         <input
           type="text"
+          placeholder="Search movies..."
           value={searchUser}
           onChange={(e) => setSearchUser(e.target.value)}
-          placeholder="Enter username"
-          className="input input-bordered w-full max-w-md text-black"
+          className="outline-none px-3 py-1 text-gray-700 w-full"
         />
-        <button onClick={handleSearch} className="btn btn-primary">
-          Search
+        <button onClick={handleSearch} className="text-green-700 p-1">
+          <IoIosSearch size={24} />
         </button>
       </div>
 
       {results.length > 0 && (
-        <ul className="mt-6">
-          {results.map((user) => (
-            <li
-              key={user._id}
-              className="p-4 shadow-sm hover:shadow-md mb-2 border rounded-md text-white"
-            >
-              <Link
-                to={`/users/${user._id}`}
-                className="font-semibold text-blue-600"
-              >
-                {user.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <>
+          <h1 className="text-xl font-bold text-center">
+            Users found: {results.length}
+          </h1>
+          <ul className="mt-6 bg-gray-900 rounded-lg">
+            {results.map((user) => (
+              <li key={user._id} className="p-4">
+                <Link
+                  to={`/users/${user._id}`}
+                  className="font-semibold text-green-500 hover:text-green-200 transition-all duration-200"
+                >
+                  {user.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </div>
   );
