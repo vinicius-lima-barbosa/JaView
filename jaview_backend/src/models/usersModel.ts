2db54875884f3307549bbbec6247d0626ad6fa07
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-25 11:36:14 -0300

    feat: add unique key in name & verify register service

--- jaview_backend/src/models/usersModel.ts
+++ jaview_backend/src/models/usersModel.ts
@@ -25,6 +25,7 @@ const userSchema = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
--- jaview_backend/src/services/userService.ts
+++ jaview_backend/src/services/userService.ts
@@ -24,6 +24,9 @@ export const createUserService = async (
     throw new Error(errorMessages.join(" "));
   }
 
+  const nameExists = await User.find({ name });
+  if (nameExists) throw new Error("Name already exists");
+
   const userExists = await User.findOne({ email });
   if (userExists) throw new Error("User already exists!");
 
