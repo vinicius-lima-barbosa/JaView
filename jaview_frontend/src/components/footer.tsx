b6e4b9982cc9b66838ac489471414e6d4914ed84
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-10-27 17:27:27 -0300

    update: update footer

--- jaview_frontend/src/components/footer.tsx
+++ jaview_frontend/src/components/footer.tsx
@@ -1,10 +1,9 @@
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 import logoTmdb from "../assets/images/logos/logoLongoTMDB.svg";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-around items-center">
+    <footer className="bg-gray-800 text-white py-8 text-sm">
+      <div className="container mx-auto flex flex-col md:flex-row justify-evenly items-center">
         <div className="flex items-center space-x-4">
           <img src={logoTmdb} alt="TMDB Logo" className="w-44" />
           <p className="text-sm">
@@ -13,54 +12,12 @@ export default function Footer() {
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 mt-6 md:mt-0">
-          <a href="/about" className="hover:text-green-400">
-            About Us
-          </a>
-          <a href="/contact" className="hover:text-green-400">
-            Contact
-          </a>
-          <a href="/privacy" className="hover:text-green-400">
-            Privacy Policy
-          </a>
-        </div>
-
-        <div className="flex space-x-4 mt-6 md:mt-0">
-          <a
-            target="_blank"
-            href="https://facebook.com"
-            className="hover:text-green-400"
-          >
-            <FaFacebookF />
-          </a>
-          <a
-            target="_blank"
-            href="https://twitter.com"
-            className="hover:text-green-400"
-          >
-            <FaTwitter />
-          </a>
-          <a
-            target="_blank"
-            href="https://instagram.com"
-            className="hover:text-green-400"
-          >
-            <FaInstagram />
-          </a>
-        </div>
-      </div>
-
-      <div className="container mx-auto mt-6 text-center">
         <p>
           Contact us:{" "}
-          <a
-            href="mailto:support@jaview.com"
-            className="text-green-400 hover:underline"
-          >
-            support@jaview.com
+          <a className="text-green-400 hover:underline">
+            viniciusbarbosa0202@gmail.com
           </a>
         </p>
-        <p>Phone: +1 (800) 123-4567</p>
       </div>
 
       <div className="text-center mt-4 text-sm">
