f0e93ee48bdd19d290c6a482a08af103fb7cc105
Andrey Dantas <andreydantasvf@gmail.com>
2025-03-22 18:07:53 -0300

    Feat: config vite server host

--- jaview_frontend/vite.config.ts
+++ jaview_frontend/vite.config.ts
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+   },
+   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+   },
 });
