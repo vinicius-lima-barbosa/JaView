f8ee9440a5b0aced51039160c8f005186650f979
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-05-15 16:47:33 -0300

    feat: add jwt-decode for token handling in FeedReviewCard and update comment deletion logic

--- jaview_frontend/src/components/feedReviewCard.tsx
+++ jaview_frontend/src/components/feedReviewCard.tsx
@@ -3,6 +3,11 @@ import { BallTriangle } from 'react-loader-spinner';
 import { FaStar, FaClock, FaTrash } from 'react-icons/fa';
 import avatarPlaceholder from '../assets/images/avatar/default_avatar.jpg';
 import { useReviewInteractions } from '../utils/useReviewInteractions';
+import { jwtDecode } from 'jwt-decode';
+
+interface JWTPayload {
+  id: string;
+}
 
 interface ReviewFeedItem {
   reviewId: string;
@@ -29,6 +34,9 @@ export function FeedReviewCard({ review }: { review: EnrichedReview }) {
     addComment,
     deleteComment
   } = useReviewInteractions(reviewId);
+
+  const token = localStorage.getItem('token');
+  const currentUserId = token ? jwtDecode<JWTPayload>(token).id : null;
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -121,18 +129,20 @@ export function FeedReviewCard({ review }: { review: EnrichedReview }) {
             {comments.map((c, index) => (
               <div
                 key={index}
-                className="flex justify-between items-start p-2 bg-gray-800 rounded"
+                className="flex justify-between items-start bg-gray-800 rounded"
               >
-                <div>
+                <div className="flex flex-col p-2">
                   <p className="text-sm text-gray-400">{c.user_id.name}</p>
                   <p className="text-white">{c.comment}</p>
                 </div>
-                <div
-                  onClick={() => deleteComment(c._id)}
-                  className="p-1 text-red-500 hover:text-red-600 cursor-pointer"
-                >
-                  <FaTrash />
-                </div>
+                {c.user_id._id === currentUserId && (
+                  <div
+                    onClick={() => deleteComment(c._id)}
+                    className="p-2 rounded-full hover:bg-slate-600 cursor-pointer"
+                  >
+                    <FaTrash className="text-slate-400 hover:text-red-600" />
+                  </div>
+                )}
               </div>
             ))}
           </div>
